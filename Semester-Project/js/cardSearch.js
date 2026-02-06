17441d0b841daa783f15babcdc659f9c7cb4b25c
Human <aptech.daniyal0@gmail.com>
2026-02-06 16:59:08 +0500

    Responsive

--- Semester-Project/js/cardSearch.js
+++ Semester-Project/js/cardSearch.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let currentCategory = 'all';
 
 function setCategory(cat, btn) {
@@ -29,4 +30,37 @@ function runFilters() {
             card.style.display = "none";
         }
     }
+=======
+let currentCategory = 'all';
+
+function setCategory(cat, btn) {
+    let buttons = document.getElementsByClassName("btn");
+    for (let b of buttons) { b.classList.remove("active"); }
+    btn.classList.add("active");
+
+    currentCategory = cat;
+    runFilters();
+}
+
+function runFilters() {
+    let searchInput = document.getElementById("searchField").value.toLowerCase();
+    let cards = document.getElementsByClassName("card");
+
+    for (let card of cards) {
+        let title = card.querySelector("h3").innerText.toLowerCase();
+        let brand = card.getAttribute("data-brand");
+
+        // Check Category Match
+        let matchCategory = (currentCategory === 'all' || brand === currentCategory);
+        // Check Search Match
+        let matchSearch = title.includes(searchInput);
+
+        // Only show if BOTH match
+        if (matchCategory && matchSearch) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    }
+>>>>>>> e04afb23c4adc869706a5e07f07f632eec3d4634
 }
--- Semester-Project/js/nav-foot.js
+++ Semester-Project/js/nav-foot.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
+$(document).ready(function () {
+    // 1. DYNAMIC ISLAND SCROLL 
+=======
 
 $(document).ready(function () {
     // 1. DYNAMIC ISLAND SCROLL LOGIC
+>>>>>>> e04afb23c4adc869706a5e07f07f632eec3d4634
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 60) {
             $('#islandContainer').addClass('scrolled');
@@ -27,4 +32,31 @@ $(document).ready(function () {
             scrollTop: $(this.hash).offset().top - 100
         }, 800);
     });
+<<<<<<< HEAD
+
+    const $mobileBtn = $('#mobileMenu');
+    const $navLinks = $('.nav-links');
+
+    $mobileBtn.on('click', function () {
+        $navLinks.toggleClass('active');
+
+        if ($navLinks.hasClass('active')) {
+            $mobileBtn.html('✕'); // Close icon
+            $('body').css('overflow', 'hidden'); // Lock scroll
+        } else {
+            $mobileBtn.html('☰'); // Menu icon
+            $('body').css('overflow', 'auto'); // Unlock scroll
+        }
+    });
+
+    // Close menu on link click
+    $('.nav-links a').on('click', function () {
+        $navLinks.removeClass('active');
+        $mobileBtn.html('☰');
+        $('body').css('overflow', 'auto');
+    });
+
 });
+=======
+});
+>>>>>>> e04afb23c4adc869706a5e07f07f632eec3d4634
